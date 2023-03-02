3f5657822d367aead9db0ee98cc5c0439876d952
Richard Ngo <richard1ngo@gmail.com>
2023-03-02 15:37:35 -0500

    add more styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,52 +2,33 @@ import logo from "../../assets/images/logo/BrainFlix-logo.svg";
 import searchIcon from "../../assets/images/icons/search.svg";
 import avatarIcon from "../../assets/images/Mohan-muruge.jpg";
 import uploadIcon from "../../assets/images/icons/upload.svg";
-import './Header.scss';
-
+import "./Header.scss";
 
 function Header() {
-    return (
-      <header>
-        <img
-          className="header__logo"
-          src={logo}
-          href="../../public/index.html"
-          alt="brainflix logo"
-        />
-        <form className="header__search" action="">
-          <button className="header__search--icon" type="submit">
-            <img
-              className="global__icon"
-              src={searchIcon}
-              alt="search bar"
-            />
-          </button>
-          <input
-            className="header__search--input"
-            type="text"
-            placeholder="Search"
-            name="search"
-          ></input>
-        </form>
-        <div className="header__avatar">
-          <img
-            className="header__avatarpic"
-            src={avatarIcon}
-            alt="avatar pic"
-          />
-        </div>
-        <form className="global__button" action="">
-        <button type="submit">
-        <img
-              className="global__icon"
-              src={uploadIcon}
-              alt="upload bar"
-            />
-          <input className="header__upload" type="file" name="filename"></input>
+  return (
+    <header className="header margin">
+      <img
+        className="header__logo"
+        src={logo}
+        href="../../public/index.html"
+        alt="brainflix logo"
+      />
+      <input
+        className="header__search"
+        type="text"
+        placeholder="Search"
+        name="search"
+      ></input>
+      <div className="header__avatar">
+        <img className="header__avatarpic" src={avatarIcon} alt="avatar pic" />
+      </div>
+        <button className="button" type="submit">
+          <img className="button__icon" src={uploadIcon} alt="upload bar" />
+          <input className="button__upload" type="file" name="filename"></input>
+          <label className="button__text" for="button__upload">UPLOAD</label>
         </button>
-        </form>
-      </header>
-    );
+    </header>
+  );
 }
 
 export default Header;
